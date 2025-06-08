8a52d36409f7b406f75db4f30814a2435fe630a5
DiandraCorreaMerlo <diandmcm@gmail.com>
2025-06-08 17:43:28 +0200

    eliminar librerias no necesarias

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="Generador_contrasenas",
     version="0.1.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    packages=find_packages(),
     install_requires=[],
     author="Diandra",
     author_email="diandmcm@gmail.com",
