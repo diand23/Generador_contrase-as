c5188972a2de109db4f235f8e8f336a0d66b4cb5
DiandraCorreaMerlo <diandmcm@gmail.com>
2025-06-08 23:45:24 +0200

    Correccon error para llamar a la libreria

--- scripts/main.py
+++ scripts/main.py
@@ -1,12 +1,2 @@
 
-from Generador_contrasenas import generar_contrasena
-
-if __name__ == "__main__":
-    password = generar_contrasena(
-        longitud=16,
-        usar_mayusculas=True,
-        usar_numeros=True,
-        usar_simbolos=True
-    )
-    print("Contraseña generada:", password)
 
