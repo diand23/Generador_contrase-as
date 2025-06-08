8347cecc5e20ad0849746ffabf89898c18621152
DiandraCorreaMerlo <diandmcm@gmail.com>
2025-06-08 12:30:52 +0200

    Importar la librería

--- scripts/main.py
+++ scripts/main.py
@@ -1,1 +1,10 @@
-from .contrasenas import generar_contrasena
+from generador_contrasenas import generar_contrasena
+
+if __name__ == "__main__":
+    password = generar_contrasena(
+        longitud=16,
+        usar_mayusculas=True,
+        usar_numeros=True,
+        usar_simbolos=True
+    )
+    print("Contraseña generada:", password)
