fd0643d80daf17bf586b0254f3a5d470ac1397f6
DiandraCorreaMerlo <diandmcm@gmail.com>
2025-06-09 16:56:18 +0200

    corrección

--- scripts/main.py
+++ scripts/main.py
@@ -2,7 +2,7 @@
 from Generador_contrasenas.contrasenas import generar_contrasena
 
 # Probar la función de generación de contraseñas
-generar_contrasena(longitud=16, usar_mayusculas=True, usar_numeros=True, usar_simbolos=True)
+password = generar_contrasena(longitud=16, usar_mayusculas=True, usar_numeros=True, usar_simbolos=True)
 # Imprimir la contraseña generada       
-print("Contraseña generada:", generar_contrasena(longitud=16, usar_mayusculas=True, usar_numeros=True, usar_simbolos=True))
+print("Contraseña generada:", password)
 
