baae8b6ef0fef07108a86acbf4e0706c350f266a
DiandraCorreaMerlo <diandmcm@gmail.com>
2025-06-12 21:55:34 +0200

    cambio de nombre de la libreria

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,8 @@
 # Importar librería
-from Generador_contrasenas.contrasenas import generar_contrasena
+from Generador_contrasenas.contrasenas import GeneradorContrasenas
 
 # Probar la función de generación de contraseñas
-password = generar_contrasena(longitud=16, usar_mayusculas=True, usar_numeros=True, usar_simbolos=True)
+password = GeneradorContrasenas(longitud=16, usar_mayusculas=True, usar_numeros=True, usar_simbolos=True)
 # Imprimir la contraseña generada       
 print("Contraseña generada:", password)
 
