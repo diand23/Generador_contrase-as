9df772f83ce2bbb43e4a571fe83ffad492e145e9
DiandraCorreaMerlo <diandmcm@gmail.com>
2025-06-13 15:49:40 +0200

    comprobación de que la librería funciona

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,15 @@
-# Importar librería
-from Generador_contrasenas.contrasenas import GeneradorContrasenas
+# Importar librerías necesarias
+from Generador_contrasenas.contrasenas import GeneradorContrasenas,EstrategiaBasica,EstrategiaSegura
 
+# Punto de entrada para la terminal
+def main():
+    generador_seguro = GeneradorContrasenas(EstrategiaSegura(), longitud=16)
+    print("Contraseña segura:", generador_seguro.generar())
+
+    generador_basico = GeneradorContrasenas(EstrategiaBasica(), longitud=10)
+    print("Contraseña básica:", generador_basico.generar())
+
+if __name__ == "__main__":
+    main()
 
 
